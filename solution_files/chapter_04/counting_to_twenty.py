ecc5f1e010931075008cbda92e9f8b4065cca7c0
Beki-Abebe <abebebeki1990@gmail.com>
2025-04-28 12:48:59 -0400

    new solution

---
+++ solution_files/chapter_04/counting_to_twenty.py
@@ -0,0 +1,7 @@
+numbers = list(range(1, 21))
+
+for number in numbers:
+    print(number)
+
+for i in range(1,21):
+    print(i)
---
+++ solution_files/chapter_04/cube_comprehension.py
@@ -0,0 +1,7 @@
+cubes = [number**3 for number in range(1,11)]
+
+for cube in cubes:
+    print(cube)
+list= []
+list.append(cubes)
+print(list)
